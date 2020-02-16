ecbc3b1750efb92db4c6f8c0127e23a70373cdd0
Shih-Ying Allen Chen <glasses618@gmail.com>
2020-02-16 11:36:08 +0800

    Change function Declaration: usd

--- chapter_1.js
+++ chapter_1.js
@@ -8,21 +8,21 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf)
 
     // 印出這筆訂單
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience} seats)\n`
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
     totalAmount += amountFor(perf)
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`
+  result += `Amount owed is ${usd(totalAmount)}\n`
   result += `You earned ${volumeCredits} credits\n`
 
   return result
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
-    }).format(aNumber)
+    }).format(aNumber / 100)
   }
 
   function volumeCreditsFor(perf) {
