f28d0c6a6e427206d802410077025582ecb9ef66
Shih-Ying Allen Chen <glasses618@gmail.com>
2020-02-16 16:07:57 +0800

    arrange project structure

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -1,8 +1,4 @@
-function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays))
-}
-
-function createStatementData(invoice, plays) {
+export default function createStatementData(invoice, plays) {
   const statementData = {}
   statementData.customer = invoice.customer
   statementData.performances = invoice.performances.map(enrichPerformances)
@@ -63,29 +59,4 @@ function createStatementData(invoice, plays) {
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
   }
-
 }
-
-function renderPlainText(data) {
-  let result = `Statement for ${data.customer}\n`
-
-  for (const perf of data.performances) {
-    // 印出這筆訂單
-    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`
-  }
-
-  result += `Amount owed is ${usd(data.totalAmount)}\n`
-  result += `You earned ${data.totalVolumeCredits} credits\n`
-
-  return result
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100)
-  }
-}
-
-module.exports = { statement }
